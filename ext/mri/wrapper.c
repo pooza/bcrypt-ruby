a76b5f6969e8687d9be4bcc33120f7171750f291
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-04-10 09:38:39 +0900

    .

--- ext/mri/wrapper.c
+++ ext/mri/wrapper.c
@@ -176,12 +176,12 @@ char *crypt_ra(const char *key, const char *setting,
 	return _crypt_blowfish_rn(key, setting, (char *)*data, *size);
 }
 
-char *crypt_r(const char *key, const char *setting, void *data)
-{
-	return _crypt_retval_magic(
-		crypt_rn(key, setting, data, CRYPT_OUTPUT_SIZE),
-		setting, (char *)data, CRYPT_OUTPUT_SIZE);
-}
+//char *crypt_r(const char *key, const char *setting, void *data)
+//{
+//	return _crypt_retval_magic(
+//		crypt_rn(key, setting, data, CRYPT_OUTPUT_SIZE),
+//		setting, (char *)data, CRYPT_OUTPUT_SIZE);
+//}
 
 char *crypt(const char *key, const char *setting)
 {
